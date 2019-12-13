8b45ca1b64b99abddd5542e56195ee0437f53748
Khakan <hakan1999@bk.ru>
2019-12-13 23:19:30 +0300

    add links for buttons

--- FirstProject/FirstProject/ViewController.swift
+++ FirstProject/FirstProject/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBAction func SoupButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func DrinkButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func SnacksButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func ColdDishesButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func HotMealsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func DessertsButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
