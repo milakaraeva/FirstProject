46cd826b3daecd0bad019bc49a85a994a07e4f79
milakaraeva <47670878+milakaraeva@users.noreply.github.com>
2019-12-13 22:25:46 +0300

    Add files via upload

---
+++ FirstProject/FirstProject/SecondView.swift
@@ -0,0 +1,9 @@
+//
+//  SecondView.swift
+//  FirstProject
+//
+//  Created by Мила Караева on 13.12.2019.
+//  Copyright © 2019 Мила Караева. All rights reserved.
+//
+
+import Foundation
---
+++ FirstProject/FirstProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Мила Караева on 07.12.2019.
+//  Copyright © 2019 Мила Караева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
